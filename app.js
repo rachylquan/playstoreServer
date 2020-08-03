31c2d0da67047815d9e17fd96dc07bb151b09007
Rachyl <rachylquan@yahoo.com>
2020-08-02 22:13:20 -0700

    initial

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common')); // let's see what 'common' format looks like
+
+const playstoreList = require('./playstore-data.js');
+
+app.get('/playstore', (req, res) => {
+  const { sort, genres = "" } = req.query;
+
+  if (sort) {
+    if (!['app', 'rating'].includes(sort)) {
+      return res
+        .status(400)
+        .send('Sort must be one of app or rating');
+    }
+  }
+
+  let results = genres 
+    ? playstoreList.filter(app => 
+        app
+          .Genres
+          .includes(genres))
+    : playstoreList;
+
+  if (sort) {
+    results
+      .sort((a, b) => {
+        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    });
+  }
+
+  res
+    .json(results);
+});
+
+app.listen(8000, () => {
+  console.log('Server started on PORT 8000');
+});
